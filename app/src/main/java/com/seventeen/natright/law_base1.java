67a896f92bd2011f2c2111e35d023e8f9baa5637
Skrewdai <jyrillisrael.marcelo.cics@ust.edu.ph>
2022-11-08 00:34:49 +0800

    First Commit - NatRight v1.0

---
+++ app/src/main/java/com/seventeen/natright/law_base1.java
@@ -0,0 +1,14 @@
+package com.seventeen.natright;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class law_base1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_law_base1);
+    }
+}
---
+++ app/src/main/java/com/seventeen/natright/organizationScreen.java
@@ -0,0 +1,14 @@
+package com.seventeen.natright;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class organizationScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_organization_screen);
+    }
+}
