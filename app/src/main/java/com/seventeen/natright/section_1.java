1f63f1ac20c33c2ff00ee445dbdfb0aa35dfab5c
kamreh <arieljohn.serrano.cics@ust.edu.ph>
2022-11-08 20:05:39 +0800

    Test

--- app/src/main/java/com/seventeen/natright/section_1.java
+++ app/src/main/java/com/seventeen/natright/section_1.java
@@ -56,29 +56,6 @@ public class section_1 extends AppCompatActivity {
         });
 
 
-        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottomNavigationView);
-
-        bottomNavigationView.setOnNavigationItemSelectedListener((item -> {
-            switch (item.getItemId())
-            {
-                case R.id.nav_Rights:
-                    startActivity(new Intent(getApplicationContext(), homeScreen.class));
-                    overridePendingTransition(0,0);
-                    return true;
-                case R.id.nav_Organization:
-                    startActivity(new Intent(getApplicationContext(), organizationScreen.class));
-                    overridePendingTransition(R.anim.slide_in_right,
-                            R.anim.slide_out_left);
-                    return true;
-
-                case R.id.nav_Information:
-                    startActivity(new Intent(getApplicationContext(), infoScreen.class));
-                    overridePendingTransition(R.anim.slide_in_right,
-                            R.anim.slide_out_left);
-                    return true;
-            }
-            return false;
-        }));
         View toolbar = findViewById(R.id.topToolbar);
         toolbar.setOnClickListener(new View.OnClickListener() {
             @Override
